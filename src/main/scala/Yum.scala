f700398526e9309cecdec2735ccb7788d52d9cf7
Alexey Alekhin <laughedelic@gmail.com>
2013-10-24 16:55:06 +0200

    Removed useless bundles

--- src/main/scala/Yum.scala
+++ src/main/scala/Yum.scala
@@ -2,18 +2,12 @@ package ohnosequences.statika
 
 import ohnosequences.statika._
 
-class YumBundle(packageName: String) extends Bundle() {
+abstract class YumBundle(packageName: String) extends Bundle() {
 
   def install[D <: AnyDistribution](d: D): InstallResults =
     Seq("yum", "install", "-y", packageName) ->- success(name + " is installed")
 
 }
-
-case object GCC extends YumBundle("gcc")
-
-case object Python extends YumBundle("python")
-
-case object ZlibDevel extends YumBundle("zlib-devel")
 
 case object Git extends YumBundle("git") {
   // TODO: check url for format correctness
