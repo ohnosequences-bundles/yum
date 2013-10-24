2401a06303f0545a39fa642c70e03ab4fdc0e8c3
Alexey Alekhin <laughedelic@gmail.com>
2013-10-24 16:08:44 +0200

    Added existing bundles as YumBundles

--- src/main/scala/Yum.scala
+++ src/main/scala/Yum.scala
@@ -2,11 +2,21 @@ package ohnosequences.statika
 
 import ohnosequences.statika._
 
-case object Yum extends Bundle() {
+class YumBundle(packageName: String) extends Bundle() {
 
-  def install[D <: AnyDistribution](d: D): InstallResults = {
-    // do someting here
-    success(name + " is installed")
-  }
+  def install[D <: AnyDistribution](d: D): InstallResults =
+    Seq("yum", "install", "-y", packageName) ->- success(name + " is installed")
 
+}
+
+case object GCC extends YumBundle("gcc")
+
+case object Python extends YumBundle("python")
+
+case object ZlibDevel extends YumBundle("zlib-devel")
+
+case object Git extends YumBundle("git") {
+  // TODO: check url for format correctness
+  def clone(repo: String, dir: String = ""): InstallResults =
+    Seq("git", "clone", repo) ++ (if (dir.isEmpty) Seq() else Seq(dir))
 }
